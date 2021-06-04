ae8fc4ff04190b54c6efdaf129aae21b16ef9449
Maxence Brette <maxbrette@gmail.com>
2021-06-04 13:32:26 -0500

    Playing around with API

--- src/helloWorldServer.py
+++ src/helloWorldServer.py
@@ -4,6 +4,8 @@ server = Flask(__name__)
 @server.route("/")
 def hello():
 	return ('Hello World')
-
+@server.route("/foo")
+def foo():
+	return ('Hello World foo')
 if __name__ == "__main__":
 	server.run(host = '0.0.0.0')
